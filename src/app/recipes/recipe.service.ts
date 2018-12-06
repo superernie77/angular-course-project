d1414adfd5ddcdeed76329308e2a4fc4225a64ae
superernie77 <superernie77@gmail.com>
2018-12-06 10:02:59 +0100

    injected services in each other

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,39 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
-    
-    recipeSelected = new EventEmitter<Recipe>();
-    
-    private recipes: Recipe[] = [
-        new Recipe('Force Pizza','extra force added','https://media.giphy.com/media/9B5EkgWrF4Rri/giphy.gif'),
-        new Recipe('Cowboy Pizza','with good milage on it','https://media.giphy.com/media/10kxE34bJPaUO4/giphy.gif')
-      ];
 
-      getRecipes(){
-          // copy of the array returned
-          return this.recipes.slice();
-      }
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe(
+            'Force Pizza',
+            'extra force added',
+            'https://media.giphy.com/media/9B5EkgWrF4Rri/giphy.gif',
+            [
+                new Ingredient('Tomatoes', 3),
+                new Ingredient('Flour', 200),
+                new Ingredient('Force', 1)
+            ]),
+        new Recipe(
+            'Cowboy Pizza',
+            'with good milage on it',
+            'https://media.giphy.com/media/10kxE34bJPaUO4/giphy.gif',
+            [new Ingredient('Tomatoes', 3),
+            new Ingredient('Cattle', 2),])
+    ];
+
+    constructor(private slService : ShoppingListService){}
+
+    getRecipes() {
+        // copy of the array returned
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients : Ingredient[]){
+        this.slService.addIngredients(ingredients);
+    }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -19,4 +19,14 @@ export class ShoppingListService {
           this.ingredients.push(ingredient);
           this.ingredientsChanged.emit(this.ingredients.slice())
       }
+
+      addIngredients(ingredients : Ingredient[]){
+        //for(let ingredient of ingredients){
+        //    this.addIngredient(ingredient);    
+        //}
+
+        // spread operator converts array into list
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients.slice())
+      }
 }
