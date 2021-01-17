e15dca31fd75b5435068df485e23e02a1e4ac590
superernie77 <superernie77@gmail.com>
2021-01-17 15:33:03 +0100

    Video 164-168 child routes and programmatic navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,9 @@ const appRoutes : Routes = [
     { path: '' , redirectTo: 'recipes' , pathMatch : 'full'},
     { path: 'recipes' , component: RecipesComponent, children : [
         {path: '', component: RecipeStartComponent },
-        {path: ':id', component : RecipeDetailComponent }
+        {path: 'new', component : RecipeDetailComponent },
+        {path: ':id', component : RecipeDetailComponent },
+        {path: ':id/edit', component : RecipeDetailComponent }
     ] },
     { path: 'shopping-list' , component: ShoppingListComponent}
 ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { DropDownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     ShoppingListComponent,
     ShoppingEditComponent,
     DropDownDirective,
-    RecipeStartComponent
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
