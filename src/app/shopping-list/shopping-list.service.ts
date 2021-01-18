3d5d1190b166b53050ed43c23f72cc93d8520ea5
superernie77 <superernie77@gmail.com>
2021-01-18 14:50:08 +0100

    Observable example

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,9 @@
 import { Ingredient } from "../shared/ingredient.model";
-import { EventEmitter } from "@angular/core";
+import { Subject } from 'rxjs';
 
 export class ShoppingListService {
 
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -17,16 +17,12 @@ export class ShoppingListService {
 
       addIngredient(ingredient : Ingredient) {
           this.ingredients.push(ingredient);
-          this.ingredientsChanged.emit(this.ingredients.slice())
+          this.ingredientsChanged.next(this.ingredients.slice())
       }
 
       addIngredients(ingredients : Ingredient[]){
-        //for(let ingredient of ingredients){
-        //    this.addIngredient(ingredient);    
-        //}
-
         // spread operator converts array into list
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice())
+        this.ingredientsChanged.next(this.ingredients.slice())
       }
 }
